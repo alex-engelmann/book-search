dbfab07f443673d76e536a4f71178f6765a9dece
Alex Engelmann <alex.engelmann@gmail.com>
2019-08-02 23:46:32 -0700

    search mostly working

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -44,27 +44,29 @@ class Books extends Component {
   handleBookSearch = event => {
     event.preventDefault();
     API.searchBooks(this.state.title)
-    .then(res => {
-      let rawresults = res.data.items;
-      let results = [];
+      .then(res => {
+        let rawresults = res.data.items;
+        let results = [];
 
-      results = rawresults.map(result => {
-        result = {
-          key: result.id,
-          id: result.id,
-          title: result.volumeInfo.title,
-          authors: (result.volumeInfo.authors.length > 1) ? result.volumeInfo.authors.join(", ") : result.volumeInfo.authors.toString(),
-          description: result.volumeInfo.description,
-          image: result.volumeInfo.imageLinks.thumbnail,
-          link: result.volumeInfo.canonicalVolumeLink
-        }
-        return result
+        //TODO some books don't have thumbnails, my ternary doesn't seem to work sometimes
+
+        results = rawresults.map(result => {
+          result = {
+            key: result.id,
+            id: result.id,
+            title: result.volumeInfo.title,
+            authors: (result.volumeInfo.authors.length > 1) ? result.volumeInfo.authors.join(", ") : result.volumeInfo.authors.toString(),
+            description: result.volumeInfo.description,
+            image: (result.volumeInfo.imageLinks) ? result.volumeInfo.imageLinks.thumbnail : "no image",
+            link: result.volumeInfo.canonicalVolumeLink
+          }
+          return result
+        })
+
+        this.setState({ searchResults: results })
+        this.setState({ title: "" });
       })
-      console.log(results);
-      this.setState({ searchResults: results })
-      this.setState({title: ""});
-  })
-    .catch(err => console.log(err)); 
+      .catch(err => console.log(err));
   };
 
   render() {
@@ -84,40 +86,62 @@ class Books extends Component {
                 placeholder="Title of book you're searching for"
               />
 
-              <FormBtn
-                disabled={!(this.state.title)}
-                onClick={this.handleBookSearch}
-              >
-                Search
+              <FormBtn disabled={!(this.state.title)}
+                onClick={this.handleBookSearch}>Search
               </FormBtn>
+              <br></br>
             </form>
+            {this.state.searchResults.length ? (
+              <List>
+                {this.state.searchResults.map(book => (
+                  <ListItem key={book.id}>
+                    
+                    <div className="card" style={{width: "100%"}}>
+                      <img src={book.image} style={{width: "30%"}} className="card-img-top" alt=""></img>
+                        <div className="card-body">
+                          <h5 className="card-title">{book.title} by {book.authors}</h5>
+                          <p className="card-text">{book.description}</p>
+                          <a href={book.link} className="btn btn-primary">Link to Google Books</a>
+                        </div>
+                    </div>
+
+
+
+                  </ListItem>
+                    ))}
+              </List>
+                ) : (
+                <h3>No Search Results (yet)</h3>
+                )}
+  
+  
           </Col>
           {/* This is all on the right part of the app */}
           <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Saved Books</h1>
-            </Jumbotron>
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.authors}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-                <h3>No Results to Display</h3>
-              )}
-          </Col>
+              <Jumbotron>
+                <h1>Saved Books</h1>
+              </Jumbotron>
+              {this.state.books.length ? (
+                <List>
+                  {this.state.books.map(book => (
+                    <ListItem key={book._id}>
+                      <Link to={"/books/" + book._id}>
+                        <strong>
+                          {book.title} by {book.authors}
+                        </strong>
+                      </Link>
+                      <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                    </ListItem>
+                  ))}
+                </List>
+              ) : (
+                  <h3>No Results to Display</h3>
+                )}
+            </Col>
         </Row>
       </Container>
-    );
-  }
-}
-
-export default Books;
+        );
+      }
+    }
+    
+    export default Books;
